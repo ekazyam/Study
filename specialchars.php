7d4abbae21b28e0752a015ce9b000f1b217d0fc5
ekazyam <readsnow77@gmail.com>
2015-04-21 23:36:42 +0900

    特殊文字のエスケープ処理

--- specialchars.php
+++ specialchars.php
@@ -1,6 +1,13 @@
 <?php
 //htmlデータを安全な文字列に変換する。
 function h($htmldata){
-	echo(htmlspecialchars( $htmldata,ENT_QUOTES,'UTF-8')."<br>");
+        echo(
+        	htmlspecialchars(
+        		$htmldata,
+			ENT_QUOTES,
+			'UTF-8'
+	)
+	."<br>"
+	);
 }
 ?>
