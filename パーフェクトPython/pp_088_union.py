df598f63f703f96debf988f3d17710f2f7821c40
ekazyam <readsnow77@gmail.com>
2015-09-13 00:19:52 +0900

    no message

--- パーフェクトPython/pp_088_union.py
+++ パーフェクトPython/pp_088_union.py
@@ -1,12 +1,12 @@
 def uniontest():
-    a = {1,2,3,4,5}
-    b = {6,7,8,9,10}
+    a = {1, 2, 3, 4, 5}
+    b = {6, 7, 8, 9, 10}
     print(a)
     print(b)
-    c =a.union(b)
+    c = a.union(b)
     print(c)
 
 if __name__ == '__main__':
     # unionは非破壊的メソッドなので、別のオブジェクトに代入しないと消えます。
     uniontest()
-    
+        print("test")
