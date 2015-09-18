c95d00c0b5f0f55cb75d851bf989301098515bab
ekazyam <readsnow77@gmail.com>
2015-09-18 22:27:09 +0900

    no message

---
+++ パーフェクトPython/pp_132_関数デコレータ.py
@@ -0,0 +1,34 @@
+import atexit
+
+
+def show_message(f):
+    """関数デコレータのテスト"""
+    print ('show_message関数です。')
+    def wrapper():
+        print("ここは関数デコレータで定義したメッセージです。")
+        return f()
+
+    return wrapper
+
+
+def show_message2(f):
+    """関数デコレータのテスト"""
+    def wrapper():
+        print("ここは関数デコレータで定義したメッセージ2です。")
+        return f()
+
+    return wrapper
+
+
+@show_message
+@show_message2
+def testfunc():
+    print('テストファンクションが呼ばれました。')
+    return 'これはリターン値です。'
+
+
+@atexit.register
+def exitfunc():
+    print('関数が終了しました。')
+
+testfunc()
