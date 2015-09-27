99775b101093fd0004b71ac299c62ce47cb4b0b0
ekazyam <readsnow77@gmail.com>
2015-09-27 14:50:15 +0900

    no message

--- パーフェクトPython/pp_085_タプル.py
+++ パーフェクトPython/pp_085_タプル.py
@@ -1,12 +1,12 @@
-x = (1,2,3,4)
-print (x)
+x = (1, 2, 3, 4)
+print(x)
 try:
-	x[0] = 5
+    x[0] = 5
 except:
-	print('タプルは値の変更を許容しません')
-	print('それ以外の挙動はlist型と同じっぽいです')
-	print('変更が不可という制約があるので、listに比較すると速度が早いっぽいです')
+    print('タプルは値の変更を許容しません')
+    print('それ以外の挙動はlist型と同じっぽいです')
+    print('変更が不可という制約があるので、listに比較すると速度が早いっぽいです')
 else:
-	pass
+    pass
 finally:
-	pass
+    pass
